fe8e00fadfe07a640810b1c65881b82b0c6d13bc
Kalea <gink@purdue.edu>
2022-12-23 01:57:15 -0500

    add commands file

---
+++ resources/commands.mk
@@ -0,0 +1,11 @@
+# Commands 
+
+vaBV - select {} block
+xp - swap 2 characters
+V$% - select function, hover on function name
+yy,c p - copy, comment, paste
+viB - select {}
+gi - jump to insert mode at last place where in insert mode
+'. - jump to first non whitespace char of line hwere chage was made
+]m  - jump to start of method block
+ctrl + o - command in insert mode	
